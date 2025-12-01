4992ef353928ffe85a780b089dadaa6ee69906be
zbabiarz <111098143+zbabiarz@users.noreply.github.com>
2025-12-01 13:13:13 -0700

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    commonjsOptions: {
+      include: [/lib/, /node_modules/],
+      transformMixedEsModules: true
+    }
+  },
+  optimizeDeps: {
+    include: ['moment-timezone']
+  }
 })
