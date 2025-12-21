f994843faeddce422d35010f574a712c1f755615
zbabiarz <111098143+zbabiarz@users.noreply.github.com>
2025-12-21 11:16:46 -0700

    Updated Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -75,7 +75,7 @@ function Home() {
       }
 
       try {
-        await fetch('https://effortlessai.app.n8n.cloud/webhook-test/06da12bd-59c3-429b-b922-344bbb94d6ed', {
+        await fetch('https://effortlessai.app.n8n.cloud/webhook-test/475b8845-0604-47ab-af7e-fe011922dcdd', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
